457c24ea4b19163237762cffa4362a7d1bb2d875
Daniel Subak <daniel.subak@klaviyo.com>
2020-01-12 16:32:47 -0500

    Working POC

--- stream_to_s3.py
+++ stream_to_s3.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 
 from optparse import OptionParser
 import sys
+import traceback
 
 # This is the minimum size for each part of a multipart upload
 FIVE_MB_IN_BYTES = 5 * 1000 * 1000
@@ -40,19 +41,29 @@ def main():
     size = 0
     multipart_upload_response = s3_client.create_multipart_upload(Bucket=s3_bucket, Key=s3_key)
     upload_id = multipart_upload_response['UploadId']
+    part_list = []
     try:
         # TODO: In testing with a ~180MB file and a 1MB chunk_size it only ran for 23 iterations.
         # That math doesn't work out; probably need to look at what's going on there.
         for chunk_number, content_chunk in enumerate(tqdm(req.iter_content(chunk_size=FIVE_MB_IN_BYTES))):
-            s3_client.upload_part(Bucket=s3_bucket, Key=s3_key, UploadId=upload_id, PartNumber=chunk_number)
+            part_upload_response = s3_client.upload_part(Body=content_chunk, Bucket=s3_bucket, Key=s3_key, UploadId=upload_id, PartNumber=chunk_number + 1)
+            part_list.append({
+                'PartNumber': chunk_number + 1,
+                'ETag': part_upload_response['ETag']
+            })
             size += len(content_chunk)
-    except Exception:
+    except Exception as exc:
         print('Caught exception, aborting multipart upload')
+        traceback.print_exc()
         s3_client.abort_multipart_upload(Bucket=s3_bucket, Key=s3_key, UploadId=upload_id)
 
-    s3_client.complete_multipart_upload(Bucket=s3_bucket, Key=s3_key, UploadId=upload_id)
 
-    print('Moved {} bytes'.format(size))
+    part_info = {
+        'Parts': part_list
+    }
+    print(s3_client.complete_multipart_upload(Bucket=s3_bucket, Key=s3_key, UploadId=upload_id, MultipartUpload=part_info))
+
+    print('Moved {} bytes in {} chunks'.format(size, chunk_number))
 
 
 
